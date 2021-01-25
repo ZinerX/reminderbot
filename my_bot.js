e558ae334780314fe0690d8485459caa91992c9e
ZinerX <zinerxcr4ft@gmail.com>
2021-01-26 00:12:08 +0800

    added conditions

--- my_bot.js
+++ my_bot.js
@@ -29,7 +29,7 @@ client.on('ready', async () => {
     timedecider = 1;
     channel.send(msgmorning);
   });
-  const scheduledMessage3 = new cron.CronJob('00 06 01 * * *', () => {
+  const scheduledMessage3 = new cron.CronJob('00 00 14 * * *', () => {
     // This runs every day at 14:00:00
     timedecider = 1;
     channel.send(cutereminder);
@@ -94,7 +94,7 @@ client.on('message', (message) => {
   }
 
   // FUNCTION LOOP HERE
-  if ((message.content.toLowerCase().includes('no') || message.content.toLowerCase().includes('deny') || message.content.toLowerCase().includes('false') || message.content.toLowerCase().includes("n't") || message.content.toLowerCase().includes(':x:')) && (message.author.id === "234108953297027073")) {
+  if ((message.content.toLowerCase().includes('no') || message.content.toLowerCase().includes('deny') || message.content.toLowerCase().includes('false') || message.content.toLowerCase().includes("n't") || message.content.toLowerCase().includes(':x:') || message.content.toLowerCase().includes('n o')) && (message.author.id === "234108953297027073")) {
     checkMessageContains();
   }
 
